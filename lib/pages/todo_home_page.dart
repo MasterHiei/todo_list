0d9d37847cdfe417ba05053b961c53151387bb65
MasterHiei <ruhrg.lcx@outlook.com>
2022-12-28 19:42:02 +0900

    refactor: minor fixes

--- lib/pages/todo_home_page.dart
+++ lib/pages/todo_home_page.dart
@@ -13,18 +13,19 @@ class TodoHomePage extends StatelessWidget {
       appBar: const TodoAppBar(),
       body: TodoListView(key: _todoListViewKey),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          showModalBottomSheet(
-            context: context,
-            builder: (_) => CreateTodoBottomSheet(
-              todoListViewKey: _todoListViewKey,
-            ),
-            isScrollControlled: true,
-            isDismissible: false,
-          );
-        },
+        onPressed: () => _showCreateTodoBottomSheet(context),
         child: const Icon(Icons.add),
       ),
     );
   }
+
+  Future<void> _showCreateTodoBottomSheet(BuildContext context) =>
+      showModalBottomSheet(
+        context: context,
+        builder: (_) => CreateTodoBottomSheet(
+          todoListViewKey: _todoListViewKey,
+        ),
+        isScrollControlled: true,
+        isDismissible: false,
+      );
 }
